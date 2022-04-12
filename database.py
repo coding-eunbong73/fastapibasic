d3161afdb9dafa4791db495c60fb36b58b511fb9
EunBong Lee <eunbong73.lee@gmail.com>
2022-04-12 16:07:31 +0900

    Change sqlite3 DB

--- database.py
+++ database.py
@@ -3,20 +3,20 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 #sqlite3
-#SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 
 #postgresql
 #SQLALCHEMY_DATABASE_URL = "postgresql://postgres:New1234!!@localhost/TodoApplicationDatabase"
 
 #mysql
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:New1234!!@127.0.0.1:3306/todoapp"
+#SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:New1234!!@127.0.0.1:3306/todoapp"
 
 engine = create_engine(
     # sqlite3
-    # SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 
     # postgresql, mysql
-    SQLALCHEMY_DATABASE_URL
+    #SQLALCHEMY_DATABASE_URL
 
 )
 
